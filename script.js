a9b7e7ac635d9103b6af990ccdd57ad567709d4d
keffri <keffri.w.neal@gmail.com>
2020-10-15 18:44:02 -0700

    create, delete, complete functionality

--- script.js
+++ script.js
@@ -8,24 +8,45 @@ let completedList = document.querySelector(".completed-list");
 /*** Create Todo ***/
 
 createTodo = () => {
-    let todo = document.createElement("li");
-    let todoValue = document.querySelector(".add-todo").value;
-    todo.textContent = todoValue;
-    if(todoValue === "") {
-        alert("Please enter a todo.")
-    } else {
-        todoList.appendChild(todo);
-    }
-    todoValue = "";
-    
-   
-    
-}
+  let todo = document.createElement("li");
+  let todoValue = document.querySelector(".add-todo").value;
+  todo.textContent = todoValue;
+
+  if (todoValue === "") {
+    return;
+  } else {
+    todoList.appendChild(todo);
+    let deleteButton = document.createElement("span");
+    deleteButton.className = "deleteButton";
+    deleteButton.textContent = "x";
+    todo.appendChild(deleteButton);
+  }
+  document.querySelector(".add-todo").value = "";
+
+  let todos = document.getElementsByTagName("li");
+  for (let i = 0; i < todos.length; i++) {
+    todos[i].addEventListener(
+      "click",
+      (add = () => {
+        todos[i].className = "completed";
+        todos[i].removeEventListener("click", add);
+        completedList.appendChild(todos[i]);
+      })
+    );
+  }
+
+  let deleteButtons = document.getElementsByClassName("deleteButton");
+  for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", () => {
+      let todo = deleteButtons[i].parentElement;
+      todo.style.display = "none";
+    });
+  }
+};
 
 /*** Dark Mode ***/
 
 let darkModeSwitch = document.querySelector("input[type=checkbox]");
-
 
 /*** Event Listeners ***/
 
